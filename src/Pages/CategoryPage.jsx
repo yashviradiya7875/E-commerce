28fd629a54d4323c8641a25e41368efd451a189f
yash viradiya <135495465+yashviradiya7875@users.noreply.github.com>
2024-10-12 11:43:31 +0530

    Update CategoryPage.jsx

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,55 +1,49 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import ImgMediaCard from '../Components/ImgMediaCard'
-import Loading from '../Components/Loading'
-
-
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ImgMediaCard from "../Components/ImgMediaCard";
+import Loading from "../Components/Loading";
 
 function CategoryPage() {
-    const { id } = useParams()
-    const [isloading, setIsloading] = useState(false)
-    const [catproduct, setCatProduct] = useState([])
+  const { id } = useParams();
+  const [isloading, setIsloading] = useState(false);
+  const [catproduct, setCatProduct] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            setIsloading(true)
-            const response = await axios.get(`https://dummyjson.com/products/category/${id}`)
-            setCatProduct(response.data.products)
-            // console.log(response.data.products)
-            setIsloading(false)
-        })()
+  useEffect(() => {
+    (async () => {
+      setIsloading(true);
+      const response = await axios.get(
+        `https://dummyjson.com/products/categories/${id}`
+      );
+      setCatProduct(response.data.products);
+      //   console.log(response.data.products);
+      setIsloading(false);
+    })();
+  }, [catproduct]);
 
-    }, [catproduct])
+  if (!catproduct) {
+    return <Loading />;
+  }
 
-    if(!catproduct){
-        return <Loading/>
-    }
-
-    return (
-        <>
-            <div className='flex flex-wrap mx-auto justify-center align-middle'>
-                {
-                    catproduct && catproduct.map(res => (
-                        <ImgMediaCard
-                            key={res.id}
-                            id={res.id}
-                            url={res.thumbnail}
-                            title={res.title}
-                            price={res.price}
-                            rating={res.rating}
-                            discount={res.discountPercentage}
-                        />
-                    ))
-                }
-            </div>
-
-
-        </>
-
-
-    )
+  return (
+    <>
+      <div className="flex flex-wrap mx-auto justify-center align-middle">
+        {catproduct &&
+          catproduct.map((res) => (
+            <ImgMediaCard
+              key={res.id}
+              id={res.id}
+              url={res.thumbnail}
+              title={res.title}
+              price={res.price}
+              rating={res.rating}
+              discount={res.discountPercentage}
+            />
+          ))}
+      </div>
+    </>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
